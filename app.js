abfec7f014ec29dd3a88bec1c5cb25cea9970e3c
WebsiteComparison <33836854+websitecomparison@users.noreply.github.com>
2017-11-20 21:58:11 +0000

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ jQuery(document).ready(function ($) {
 	Bootstrap Datepicker
 	https://github.com/eternicode/bootstrap-datepicker
 	 */
-	form.find('.input-group.date').datepicker(format: 'dd/mm/yyyy');
+	form.find('.input-group.date').datepicker();
 
 	/*
 	Editing the form
